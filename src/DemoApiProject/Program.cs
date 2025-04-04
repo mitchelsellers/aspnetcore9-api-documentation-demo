a401abff0e8419e7e31fc100362b869864f3c069
Mitchel Sellers <msellers@iowacomputergurus.com>
2025-04-04 08:32:29 -0500

    Initial project Setup (Open API Only)

---
+++ src/DemoApiProject/Program.cs
@@ -0,0 +1,34 @@
+
+namespace DemoApiProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi(); // Served from here: https://localhost:7062/openapi/v1.json
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
